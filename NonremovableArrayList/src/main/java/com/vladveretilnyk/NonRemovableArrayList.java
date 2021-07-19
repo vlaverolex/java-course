23a4f389349036681cc9b9a041ae9ce09c37c8b2
vlaverolex <vladveretilnyk@gmail.com>
2021-07-20 00:27:37 +0300

    commit v2

--- NonremovableArrayList/src/main/java/com/vladveretilnyk/NonRemovableArrayList.java
+++ NonremovableArrayList/src/main/java/com/vladveretilnyk/NonRemovableArrayList.java
@@ -2,19 +2,18 @@ package com.vladveretilnyk;
 
 import java.util.AbstractList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class NonRemovableArrayList<T> extends AbstractList<T> {
     private Object[] values;
-    private int size;
 
     public NonRemovableArrayList() {
         values = new Object[]{};
     }
 
     public boolean add(T element) {
-        if (size == values.length) values = grow();
-        values[size] = element;
-        size++;
+        values = grow();
+        values[values.length - 1] = element;
         return true;
     }
 
@@ -29,6 +28,25 @@ public class NonRemovableArrayList<T> extends AbstractList<T> {
 
     @Override
     public int size() {
-        return size;
+        return values.length;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Itr();
+    }
+
+    private class Itr implements Iterator<T> {
+        private int cursor;
+
+        @Override
+        public boolean hasNext() {
+            return cursor != values.length;
+        }
+
+        @Override
+        public T next() {
+            return (T) values[cursor++];
+        }
     }
 }
